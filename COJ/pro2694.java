ffc829b1d951628cb86c4e31815329a0acaefaf3
David Jacobo <jguillen@cimat.mx>
2015-02-10 22:34:20 -0600

    Adding ad hoc code

---
+++ COJ/pro2694.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class pro2694{
+    public static void main(String[] args){
+        System.out.println("This is my first solution in programming competitions");
+    }
+}
