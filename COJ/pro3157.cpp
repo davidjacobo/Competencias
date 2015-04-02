4249c3227927efff6b78f0c2d3f97c9bf21f78cd
davidjacobo <jguillen@cimat.mx>
2015-04-01 18:38:56 -0600

    dp subsequence counting

--- COJ/pro3157.cpp
+++ COJ/pro3157.cpp
@@ -4,26 +4,36 @@
 #include <algorithm>
 #define MAX_N 100002
 using namespace std;
-char cow[] = "COW";
-char word[MAX_N];
-int counter;
 
-void dfs(int ind,int i,int len){
-    if(ind==3){
-        counter++;
-        return;
+typedef long long int lli;
+char cow[] = " COW";
+char word[MAX_N];
+
+lli dp(int len){
+    lli mat[4][len];
+    memset(mat, 0, 4*len*sizeof(lli));
+    for(int i=1;i<len;i++)
+        mat[1][i] = mat[1][i-1] + ((word[i]=='C')?1:0);
+
+    for(int i=2;i<4;i++){
+        int counter = 0;
+        for(int j=1;j<len;j++){
+            if(word[j]==cow[i]){
+                counter++;
+                mat[i][j] = mat[i-1][j-1] + mat[i][j-1];
+            }
+            else
+                mat[i][j] = mat[i][j-1];
+        }
     }
 
-    char car = cow[ind];
-    for(int j=i;j<len;j++)
-        if(car==word[j]) dfs(ind+1,j+1,len);
+    return mat[3][len-1];
 }
 
 int main(){
     int len;
-    scanf("%d %s", &len, word);
-    counter = 0;
-    dfs(0,0,len);
-    printf("%d\n",counter);
+    word[0] = ' ';
+    scanf("%d %s", &len, &word[1]);
+    printf("%lld\n",dp(len+1));
     return 0;
 }
