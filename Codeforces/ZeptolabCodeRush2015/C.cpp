b24f2135bcf759d23e74d1a3e683d58b126690d4
davidjacobo <jguillen@cimat.mx>
2015-04-05 16:01:17 -0500

    Math, simulation

--- Codeforces/ZeptolabCodeRush2015/C.cpp
+++ Codeforces/ZeptolabCodeRush2015/C.cpp
@@ -1,25 +1,13 @@
 #include <cstdio>
+#include <cmath>
 using namespace std;
 typedef long long int lli;
 
-lli joy(int Hr, int x,int Hb,int y){
-    return (lli)Hr*x+(lli)Hb*y;
-}
-
-lli weight(int Wr,int x,int Wb,int y){
-    return (lli)(Wr*x+Wb*y);
-}
-
-int x_y(int x,int Wr,int C,int Wb){
-    lli total = (lli)x*Wr;
-    if(total > C) return -1;
-    return (int)((C-total)/Wb);
-}
-
 int main(){
     int C,Hr,Hb,Wr,Wb;
+    int max_red;
     scanf("%d %d %d %d %d",&C,&Hr,&Hb,&Wr,&Wb);
-    if(Wb>Wr){
+    if(C/Wb<2000){
         Hr^=Hb;
         Hb^=Hr;
         Hr^=Hb;
@@ -27,7 +15,21 @@ int main(){
         Wb^=Wr;
         Wr^=Wb;
         Wb^=Wr;
+        max_red = C;
+    } else if(C/Wr<2000) {
+        max_red = C;
+    } else if((lli)Wb*Hr>(lli)Wr*Hb) {
+        Hr^=Hb;
+        Hb^=Hr;
+        Hr^=Hb;
+
+        Wb^=Wr;
+        Wr^=Wb;
+        Wb^=Wr;
+        max_red = sqrt(C)+1;
         //reducing range, if possible
+    } else {
+        max_red = sqrt(C)+1;
     }
 
     if(Wb==Wr){
@@ -36,15 +38,16 @@ int main(){
             Hb = Hr;
         printf("%lld\n",(lli)(C/Wb)*Hb);
     } else {
-        int x,y;
-        lli max_joy = 0, joyt;
-        for(int i=0;i<=C;i++){
-            x = i;
-            y = x_y(x,Wr,C,Wb);
-            if(y<0) break;
-            joyt = joy(Hr,x,Hb,y);
-            printf("x:%d y:%d joy:%lld\n",x,y,joyt);
+        int y,weight_r=0;
+        lli max_joy = 0, joyt, red_sum=0;
+        for(int i=0;i<=max_red && weight_r<=C;i++){
+            y = (C-weight_r)/Wb;
+            joyt = red_sum+(lli)y*Hb;
+            //printf("x:%d y:%d joy:%lld\n",i,y,joyt);
             if(joyt > max_joy) max_joy = joyt;
+
+            weight_r+= Wr;
+            red_sum+= Hr;
         }
         printf("%lld\n",max_joy);
     }
