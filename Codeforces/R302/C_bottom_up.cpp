54032341596e543c42ce750663ae727971ee644d
davidjacobo <jguillen@cimat.mx>
2015-05-27 20:52:53 -0500

    DP O(n3), using windowing to evade TLE

--- Codeforces/R302/C_bottom_up.cpp
+++ Codeforces/R302/C_bottom_up.cpp
@@ -11,23 +11,12 @@ int dp(){
     for(int i=1;i<=N;++i){
         for(int j=0;j<=M;++j){
             for(int k=0;k<=F;++k){
-                if(last[j][k]!=-1){
-                    //try to sum coder i as much as possible, project to nuevo
-                    nuevo[j][k] = (nuevo[j][k]+last[j][k]) % mod;
-                    int written_lines = j+1;
-                    int failures = k + lines[i];
-
-                    while(written_lines <= M && failures<=F){
-                        nuevo[written_lines][failures] = (nuevo[written_lines][failures] + last[j][k]) % mod;
-
-                        written_lines++;
-                        failures+= lines[i];
-                    }
-                }
+                nuevo[j][k] = last[j][k];
+                if(k >= lines[i] && j>0)
+                    nuevo[j][k]= (nuevo[j-1][k-lines[i]] + nuevo[j][k]) % mod;
             }
         }
         for(int l=0;l<=M;++l) for(int p=0;p<=F;++p) last[l][p] = nuevo[l][p];
-        memset(nuevo, 0, sizeof nuevo);
     }
     
     int ans = 0;
@@ -44,7 +33,7 @@ int main(){
         //cin>>lines[i];
         scanf("%d",&lines[i]);
 
-    memset(last, -1, sizeof last);
+    memset(last, 0, sizeof last);
     memset(nuevo, 0, sizeof nuevo);
 
 
